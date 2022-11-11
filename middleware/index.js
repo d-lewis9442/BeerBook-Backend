deff590e85a7eb6cb3d3e9a5b30edd0c4e6756d8
Max Calabro <maxccalabro@gmail.com>
2022-11-11 12:18:36 -0500

    started auth, hash and compare password and createToken

--- middleware/index.js
+++ middleware/index.js
@@ -4,3 +4,18 @@ require('dotenv').config()
 
 const SALT_ROUNDS = parseInt(process.env.SALT_ROUNDS)
 const APP_SECRET = process.env.APP_SECRET
+
+const hashPassword = async (password) => {
+  let hashedPassword = await bcrypt.hash(password, SALT_ROUNDS)
+  return hashPassword
+}
+
+const comparePassword = async (storedPassword, password) => {
+  let passwordMatch = await bcrypt.compare(password, storedPassword)
+  return passwordMatch
+}
+
+const createToken = (payload) => {
+  let token = jwt.sign(payload, APP_SECRET)
+  return token
+}
