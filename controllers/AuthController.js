2bc7da133130d81878ed84894eb98fa7a2c5eb84
Max Calabro <maxccalabro@gmail.com>
2022-11-11 13:46:46 -0500

    login works

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -3,6 +3,22 @@ const middleware = require('../middleware')
 
 const Login = async (req, res) => {
   try {
+    const user = await User.findOne({
+      where: { email: req.body.email },
+      raw: true
+    })
+    if (
+      user &&
+      (await middleware.comparePassword(user.passwordDigest, req.body.password))
+    ) {
+      let payload = {
+        id: user.id,
+        email: user.email
+      }
+      let token = middleware.createToken(payload)
+      return res.send({ user: payload, token })
+    }
+    res.status(401).send({ status: 'Error', msg: 'Unauthorized' })
   } catch (error) {
     throw error
   }
