5bba5aa7853d03a90a6d8467d172af7449c04815
Max Calabro <maxccalabro@gmail.com>
2022-11-11 14:25:41 -0500

    UpdatePassword working with auth

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -35,7 +35,29 @@ const Register = async (req, res) => {
   }
 }
 
+const UpdatePassword = async (req, res) => {
+  try {
+    const user = await User.findOne({
+      where: { email: req.body.email }
+    })
+    if (
+      user &&
+      (await middleware.comparePassword(
+        user.passwordDigest,
+        req.body.oldPassword
+      ))
+    ) {
+      let passwordDigest = await middleware.hashPassword(req.body.newPassword)
+      await user.update({ passwordDigest })
+      return res.send({ status: 'Sucess', msg: 'Password Updated' })
+    }
+    res.status(401).send({ status: 'Error', msg: 'Unauthorized' })
+  } catch (error) {
+    throw error
+  }
+}
 module.exports = {
   Login,
-  Register
+  Register,
+  UpdatePassword
 }
