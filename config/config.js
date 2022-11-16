f27a6a93d8d2c4e37edbbf9330552a2e4f5c1821
Max Calabro <maxccalabro@gmail.com>
2022-11-16 10:05:42 -0500

    heroku setup

--- config/config.js
+++ config/config.js
@@ -2,17 +2,14 @@ require('dotenv').config()
 module.exports = {
   development: {
     database: 'beerbook_development',
-    //"host": "127.0.0.1",
     dialect: 'postgres'
   },
   test: {
     database: 'beerbook_test',
-    //"host": "127.0.0.1",
     dialect: 'postgres'
   },
   production: {
-    database: 'beerbook_production',
-    //"host": "127.0.0.1",
+    use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
     dialectOptions: {
       ssl: {
