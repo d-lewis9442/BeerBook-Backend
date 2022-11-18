492da27aff70afefbf27a1a108cb2cba75d97706
Max Calabro <maxccalabro@gmail.com>
2022-11-18 12:15:18 -0500

    cleaned up code

--- models/beerlist.js
+++ models/beerlist.js
@@ -2,13 +2,7 @@
 const { Model } = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
   class BeerList extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
       BeerList.belongsTo(models.User, { as: 'creator', foreignKey: 'userId' })
       BeerList.belongsToMany(models.Beer, {
         through: 'beer_list_beers',
--- models/beerlistbeers.js
+++ models/beerlistbeers.js
@@ -2,14 +2,7 @@
 const { Model, ARRAY } = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
   class BeerListBeers extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
+    static associate(models) {}
   }
   BeerListBeers.init(
     {
